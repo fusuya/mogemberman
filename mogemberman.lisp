ea7581d0e0ed8e79d5a070481894352e4e07b553
fusuya <monster_dogoran@yahoo.co.jp>
2020-08-09 15:14:20 +0900

    update

--- mogemberman.lisp
+++ mogemberman.lisp
@@ -6,8 +6,8 @@
 
 (in-package moge)
 
-(defvar *window-w* 1024)
-(defvar *window-h* 728)
+(defparameter *window-w* 800)
+(defparameter *window-h* 600)
 (defparameter *tate* 12) ;;13
 (defparameter *yoko* 16) ;;17
 (defvar *white* (vec4 1 1 1 1))
@@ -19,15 +19,39 @@
 (defvar *game* nil)
 (defvar *player* nil)
 (defvar *keystate* nil)
+(defparameter *gamespeed-list* (list 60 30 15 7 1))
+(defparameter *gamespeed-count* 0)
+(defparameter *draw-gamespeed-list* (list "通常" "2倍速" "4倍速" "8倍速" "高速"))
+(defparameter *frame* 0)
+(defparameter *frame-go* 60)
+
+(defparameter *falling-wall*
+  '((1 11) (2 11) (3 11) (4 11) (5 11) (6 11) (7 11) (8 11) (9 11)
+    (10 11) (11 11) (12 11) (13 11) (14 11) (15 11) (15 10) (15 9) (15 8)
+    (15 7) (15 6) (15 5) (15 4) (15 3) (15 2) (15 1) (14 1) (13 1) (12 1)
+    (11 1) (10 1) (9 1) (8 1) (7 1) (6 1) (5 1) (4 1) (3 1) (2 1) (1 1)
+    (1 2) (1 3) (1 4) (1 5) (1 6) (1 7) (1 8) (1 9) (1 10)
+    (2 10) (3 10) (4 10) (5 10) (6 10) (7 10) (8 10) (9 10) (10 10)
+    (11 10) (12 10) (13 10) (14 10)
+    (14 9) (14 8) (14 7) (14 6) (14 5) (14 4) (14 3) (14 2)
+    (13 2) (12 2) (11 2) (10 2) (9 2) (8 2) (7 2) (6 2) (5 2) (4 2) (3 2) (2 2)
+    (2 3) (2 4) (2 5) (2 6) (2 7) (2 8) (2 9)
+    ;;(3 9) (4 9) (5 9) (6 9) (7 9) (8 9) (9 9) (10 9) (11 9) (12 9) (13 9)
+    ;;(13 8) (13 7) (13 6) (13 5) (13 4) (13 3)
+    ;;(12 3) (11 3) (10 3) (9 3) (8 3) (7 3) (6 3) (5 3) (4 3) (3 3)
+    ;;(3 4) (3 5) (3 6) (3 7) (3 8)
+    ))
+	
 
 (defclass game ()
-  ((walls     :initarg :walls     :initform nil :accessor walls)
-   (blocks      :initarg :blocks      :initform nil :accessor blocks)
+  ((walls    :initarg :walls    :initform nil :accessor walls)
+   (blocks   :initarg :blocks   :initform nil :accessor blocks)
    (players  :initarg :players  :initform nil :accessor players)
    (bombs    :initarg :bombs    :initform nil :accessor bombs)
    (items    :initarg :items    :initform nil :accessor items)
    (fires    :initarg :fires    :initform nil :accessor fires)
-   (turn     :initarg :turn     :initform nil :accessor turn)
+   (turn     :initarg :turn     :initform 1   :accessor turn)
+   (control  :initarg :control  :initform nil :accessor control)
    ))
 
 (defclass player ()
@@ -36,16 +60,19 @@
    (img          :initarg :img          :initform 0   :accessor img)
    (isalive      :initarg :isalive      :initform t   :accessor isalive)
    (name         :initarg :name         :initform nil :accessor name)
+   (playing      :initarg :playing      :initform nil :accessor playing)
+   (kick         :initarg :kick         :initform nil :accessor kick)
    (action       :initarg :action       :initform nil :accessor action)
    (power        :initarg :power        :initform 2   :accessor power)
-   (actmove      :initarg :actmove      :initform 2   :accessor actmove)
-   (actbomb      :initarg :actbomb      :initform 2   :accessor actbomb)
+   (actmove      :initarg :actmove      :initform "STAY"   :accessor actmove)
+   (actbomb      :initarg :actbomb      :initform "false"   :accessor actbomb)
    (setbomblimit :initarg :setbomblimit :initform 2   :accessor setbomblimit)
    (setbombcount :initarg :setbombcount :initform 0   :accessor setbombcount)))
 
 (defclass bomb ()
    ((pos     :initarg :pos    :initform nil :accessor pos)
     (timer   :initarg :timer  :initform 0   :accessor timer)
+    (kicked  :initarg :kicked :initform nil :accessor kicked)
     (power   :initarg :power  :initform t   :accessor power)))
 
 (defclass fire ()
@@ -68,6 +95,9 @@
    (down   :initarg :down  :initform nil   :accessor down)
    (right  :initarg :right :initform nil   :accessor right)
    (left   :initarg :left  :initform nil   :accessor left)
+   (lshift :initarg :lshift  :initform nil   :accessor lshift)
+   (spc    :initarg :spc   :initform nil   :accessor spc)
+   (rkey   :initarg :rkey   :initform nil   :accessor rkey)
    (zkey   :initarg :zkey  :initform nil   :accessor zkey)))
 
 (gamekit:register-resource-package :keyword "img/")
@@ -81,6 +111,15 @@
 (define-image :fire "fire.png")
 (define-image :fireup "fireup.png")
 (define-image :bombup "bombup.png")
+(define-image :kick   "kick.png")
+(define-font :mplus "mplus-1mn-regular.ttf")
+
+(defparameter *status-font* nil)
+(defparameter *gamespeed-font* nil) 
+
+(defun set-font ()
+  (setf *status-font* (make-font :mplus 28)
+	*gamespeed-font*  (make-font :mplus 32)))
 
 (gamekit:defgame mogemberman () ()
   (:viewport-width *window-w*)
@@ -113,47 +152,30 @@
         (subseq line 0 (1+ last-char-pos))
       "")))
 
-(defun get-ai-from-txt ()
-  (with-open-file (in "ai.txt")
-    (loop :for line = (read-line in nil)
-       :for pos :in (list (list 1 11) (list 15 11) (list 1 1) (list 15 1))
-       :for img :in (list :p1 :p2 :p3 :p4)
-       :for id :from 0 :to 3
-       :while line
-       :do (print id)
-	 (load-ai id pos img (chomp (format nil "~a" line))))))
 
 (define-condition handshake-error (error) ())
-
-
-
 ;;ai.txtからai起動するコマンドを読み込む
 ;;*ai* ストリーム？
 (defun load-ai (id pos img command-line)
   (let* ((hoge (ppcre:split #\space command-line))
 	 (atama nil)
 	 (com (make-instance 'com :pos pos :id id :img img)))
-    (format t "MOGE")
     (setf (proc com) (sb-ext:run-program
 		      (car hoge) (cdr hoge)
 		      :input :stream
 		      :output :stream
 		      :wait nil
 		      :search t))
-    (format t "HOGE")
     (setf (ai-stream com) (make-two-way-stream (sb-ext:process-output (proc com))
 					       (sb-ext:process-input (proc com))))
-    (format t "NOGE")
     (handler-case
      (setf (name com) (read-line (ai-stream com)))
      (end-of-file (c)
                   (format t "~A~%" c)
                   (error 'handshake-error)))
-    (format t "KOGE")
     (when (equal (name com) "")
       (format t "AIの名前が空です。~%")
       (error 'handshake-error))
-    (format t "GOGE")
     (setf atama (char (name com) 0))
     (cond
       ((= 2 (moge-char-width atama))
@@ -162,12 +184,37 @@
        (setf (atama com) (format nil "~c" (code-char (+ 65248 (char-code atama))))))
       (t
        (setf (atama com) "主")))
-    (format t "LOGE")
     (format (ai-stream com) "~d~%" id)
     (finish-output (ai-stream com))
-    (format t "POGE")
-    (push com (players *game*))
-    (format t "HOGE2~%")))
+    (setf (players *game*) (append (players *game*) (list com)))))
+
+;;使用するai読み込む3体以下なら操作キャラを追加
+(defun get-ai-from-txt ()
+  (with-open-file (in "ai.txt")
+    (loop :for line = (read-line in nil)
+       :for pos :in (list (list 1 11) (list 15 11) (list 1 1) (list 15 1))
+       :for img :in (list :p1 :p2 :p3 :p4)
+       :for id :from 0 :to 3
+       :while line
+       :do (print id)
+	 (load-ai id pos img (chomp (format nil "~a" line)))))
+  ;;aiが3人以下なら操作キャラを追加
+  (let ((len (length (players *game*)))
+	(p nil))
+    (when (> 4 len)
+      (case len
+	(1 (setf p (make-instance 'player :pos (list 15 11) :id 2 :img :p2)))
+	(2 (setf p (make-instance 'player :pos (list 1 1) :id 3 :img :p3)))
+	(3 (setf p (make-instance 'player :pos (list 15 1) :id 4 :img :p4))))
+      (setf (playing p) t
+	    (name p) "あなた"
+	    (control *game*) t)
+      (setf (players *game*) (append (players *game*) (list p))))))
+
+
+
+
+
 
 
 (defun create-field ()
@@ -193,13 +240,28 @@
 			(>= (random 7) 1))
 		   (push (list x y) (blocks *game*)))))))
 
+(defun init-players-data ()
+  (loop :for p :in (players *game*)
+     :for pos :in (list (list 1 11) (list 15 11) (list 1 1) (list 15 1))
+     :do (setf (pos p) pos
+	       (power p) 2
+	       (setbomblimit p) 2
+	       (setbombcount p) 0
+	       (kick p) nil
+	       (isalive p) t
+	       (actmove p) "STAY"
+	       (actbomb p) "false")))
 
-
-
-(defun create-players ()
-  (push
-   (make-instance 'player :pos '(1 1) :name "も")
-   (players *game*)))
+(defun init-data ()
+  (setf (walls *game*) nil
+	(blocks *game*) nil
+	(bombs *game*) nil
+	(fires *game*) nil
+	(items *game*) nil
+	(turn *game*) 1)
+  (init-players-data)
+  (create-field ))
+  
 
 (defun draw-background ()
   (draw-rect (vec2 0 0) *window-w* *window-h* :fill-paint *green*))
@@ -213,7 +275,25 @@
        :do (draw-image (vec2 (* (car w) 32) (* (cadr w) 32)) :block))))
 
 
-(defun draw-player (p)
+(defun draw-gamespeed ()
+  (draw-text (format nil "ゲーム速度:~a (左shiftで変更)"
+		     (nth *gamespeed-count* *draw-gamespeed-list*))
+	     (vec2 260 500) :font *gamespeed-font* )
+  (draw-text "rキーで再戦(初期化)" (vec2 260 460) :font *gamespeed-font*))
+
+;;ステータス表示
+(defun draw-com-status (p status-y)
+  (let ((k (if (kick p) "あり" "なし")))
+    (draw-rect (vec2 550 (- status-y 50)) 240 90 :stroke-paint *black*)
+    (draw-image (vec2 550 status-y) (img p))
+    (draw-text (format nil "名前:~a" (name p)) (vec2 590 (+ status-y 20)) :font *status-font*)
+    (draw-text (format nil "ボム:~d" (setbomblimit p)) (vec2 590 (- status-y 0)) :font *status-font*)
+    (draw-text (format nil "火力:~d" (power p)) (vec2 590 (- status-y 20)) :font *status-font*)
+    (draw-text (format nil "キック:~a" k) (vec2 590 (- status-y 40)) :font *status-font*)
+    ))
+
+(defun draw-player (p status-y)
+  (draw-com-status p status-y)
   (when (isalive p)
     (let* ((x (car (pos p)))
 	   (y (cadr (pos p))))
@@ -221,15 +301,9 @@
 
 (defun draw-players ()
   (loop :for p :in (players *game*)
-     :do (draw-player p)))
+       :for status-y :from 380 :downto 0 :by 100
+     :do (draw-player p status-y)))
 
-(defun draw-test ()
-  (with-slots (up down left right) *keystate*
-    (cond
-      (up    (draw-image (vec2 100 100) :fire :origin (vec2 0 64) :width 32 :height 32  ))
-      (down  (draw-image (vec2 200 100) :wall))
-      (right (draw-image (vec2 100 200) :wall))
-      (left  (draw-image (vec2 200 200) :wall)))))
 
 
 (defun draw-bomb (b)
@@ -260,25 +334,18 @@
        :do (draw-item item)))
 
 
-(defun update-player ()
+(defun update-player (p)
   (with-slots (up down left right zkey) *keystate*
-    (let ((hoge (copy-list (pos *player*))))
-      (when (and zkey
-		 (> (setbomblimit *player*) (setbombcount *player*)))
-	(push 
-	 (make-instance 'bomb :timer 10 :power (power *player*)
-			:pos hoge)
-	 (bombs *game*)))
-      (cond
-	(up    (incf (cadr (pos *player*))))
-	(down  (decf (cadr (pos *player*))))
-	(right (incf (car (pos  *player*))))
-	(left  (decf (car (pos  *player*)))))
-      (when (or (find (pos *player*) (blocks *game*) :test #'equal)
-		(find (pos *player*) (walls *game*) :test #'equal)
-		(find (pos *player*) (bombs *game*) :key #'pos :test #'equal))
-	(setf (pos *player*) hoge))
-      )))
+    (when (and zkey
+	       (> (setbomblimit p) (setbombcount p)))
+      (setf (actbomb p) "true")
+      (incf (setbombcount p)))
+    (cond
+      (up    (setf (actmove p) "UP"))
+      (down  (setf (actmove p) "DOWN"))
+      (right (setf (actmove p) "RIGHT"))
+      (left  (setf (actmove p) "LEFT"))
+      (t (setf (actmove p) "STOP")))))
 
 
 ;;アイテム追加
@@ -291,7 +358,7 @@
      (push (make-instance 'item :pos pos :name "弾" :img :bombup)
 	   (items *game*)))
     (4
-     (push (make-instance 'item :pos pos :name "蹴" :img :bombup)
+     (push (make-instance 'item :pos pos :name "蹴" :img :kick)
 	   (items *game*)))))
 		
 
@@ -342,12 +409,32 @@
 			 (fires *game*))
 		   (setf y-flag nil)))))))
 
+;;壁とブロックとボムとの当たり判定
+(defun collision-blocks-walls-boms (pos)
+  (or (find pos (blocks *game*) :test #'equal)
+      (find pos (walls *game*) :test #'equal)
+      (find pos (bombs *game*) :key #'pos :test #'equal)))
+
+
 (defun update-bomb (b)
   (decf (timer b))
-  (when (= (timer b) 0)
-    (create-fire b)
-    (setf (bombs *game*)
-	  (remove b (bombs *game*) :test #'equal))))
+  (cond
+    ((= (timer b) 0)
+     (create-fire b)
+     (setf (bombs *game*)
+	   (remove b (bombs *game*) :test #'equal)))
+    ((kicked b)
+     (let ((temp (copy-list (pos b))))
+       (cond 
+	 ((string= (kicked b) "UP")    (incf (cadr (pos b))))
+	 ((string= (kicked b) "DOWN")  (decf (cadr (pos b))))
+	 ((string= (kicked b) "RIGHT") (incf (car (pos b))))
+	 ((string= (kicked b) "LEFT")  (decf (car (pos b)))))
+       (when (or (find (pos b) (blocks *game*) :test #'equal)
+		 (find (pos b) (walls *game*) :test #'equal)
+		 (find (pos b) (remove b (bombs *game*) :test #'equal) :key #'pos :test #'equal))
+	 (setf (pos b) temp
+	       (kicked b) nil))))))
 
 (defun update-bombs ()
   (loop :for bomb :in (bombs *game*)
@@ -403,7 +490,9 @@
   (when (string= (actbomb com) "true")
     (push (make-instance 'bomb :timer 10 :power (power com)
 			 :pos (copy-list (pos com)))
-	  (bombs *game*))))
+	  (bombs *game*))
+    (when (playing com)
+      (setf (actbomb com) "false"))))
 
 ;;comの移動
 (defun update-com-position (com)
@@ -415,18 +504,30 @@
       ((string= dir "UP") (incf (cadr (pos com))))
       ((string= dir "DOWN") (decf (cadr (pos com))))
       ((string= dir "LEFT") (decf (car (pos com))))
-      ((string= dir "RIGHT") (incf (car (pos com)))))
-    (when (or (find (pos com) (blocks *game*) :test #'equal)
-	      (find (pos com) (walls *game*) :test #'equal)
-	      (find (pos com) (bombs *game*) :key #'pos :test #'equal))
-	(setf (pos com) temp))))
+      ((string= dir "RIGHT") (incf (car (pos com))))
+      (t nil))
+    (let ((bomb (find (pos com) (bombs *game*) :key #'pos :test #'equal)))
+      (cond
+	((or (find (pos com) (blocks *game*) :test #'equal)
+	     (find (pos com) (walls *game*) :test #'equal))
+	 (setf (pos com) temp))
+	((and bomb (kick com))
+	 (setf (pos com) temp
+	       (kicked bomb) dir))
+	(bomb
+	 (setf (pos com) temp))))
+    ;;操作キャラ移動方向初期化
+    (when (playing com)
+      (setf (actmove com) "STOP"))))
   
 (defun update-coms ()
   (let ((data (create-send-data)))
     (loop :for com :in (players *game*)
        :do (when (isalive com)
-	     (send-data-to-com com data)
-	     (get-data-from-com com)))
+	     (if (playing com)
+		 (update-player com)
+		 (progn (send-data-to-com com data)
+			(get-data-from-com com)))))
     (loop :for com :in (players *game*)
        :do (when (isalive com)
 	     (update-com-put-bomb com)))
@@ -434,54 +535,124 @@
        :do (when (isalive com)
 	     (update-com-position com)))))
 
+
+(defun draw-turn ()
+  (draw-text (format nil "TURN:~d" (turn *game*))
+	     (vec2 0 430) :font *gamespeed-font*))
+
 (defmethod gamekit:draw ((app mogemberman))
   (draw-background)
+  (draw-gamespeed )
   (draw-field)
-  (draw-bombs)
-  (draw-fires)
   (draw-items)
-  (draw-test)
-  (draw-players))
+  (draw-fires)
+  (draw-bombs)
+  (draw-players)
+  (draw-turn))
 ;;(draw-player *player*))
 
+;;アイテムとキャラの当たり判定
+(defun collision-item-com ()
+  (loop :for p :in (players *game*)
+     :do (let ((item (find (pos p) (items *game*) :key #'pos :test #'equal)))
+	   (when item
+	     (cond
+	       ((eq (img item) :bombup) (incf (setbomblimit p)))
+	       ((eq (img item) :fireup) (incf (power p)))
+	       ((eq (img item) :kick) (setf (kick p) t)))
+	     (setf (items *game*)
+		   (remove item (items *game*) :test #'equal))))))
 
 (defun collision-fire-com ()
   (loop :for p :in (players *game*)
      :do (when (find (pos p) (mapcar #'pos (fires *game*)) :test #'equal)
 	   (setf (isalive p) nil))))
 
+(defun collision-fire-item ()
+  (loop :for item :in (items *game*)
+     :do (when (find (pos item) (mapcar #'pos (fires *game*)) :test #'equal)
+	   (setf (items *game*)
+		 (remove item (items *game*) :test #'equal)))))
+
+
 ;;fire-bombの当たり判定
 (defun collision-fire-bomb ()
-  (loop :for bomb :in (bombs *game*)
-     :do (when (find (pos bomb) (mapcar #'pos (fires *game*)) :test #'equal)
-	   (create-fire bomb)
-	   (setf (bombs *game*)
-		 (remove bomb (bombs *game*) :test #'equal)))))
+  (let ((hoge nil))
+    (loop :for bomb :in (bombs *game*)
+       :do (when (find (pos bomb) (mapcar #'pos (fires *game*)) :test #'equal)
+	     (create-fire bomb)
+	     (setf hoge t)
+	     (setf (bombs *game*)
+		   (remove bomb (bombs *game*) :test #'equal))))
+    (when hoge
+      (collision-fire-bomb))))
 
 
 (defun delete-fires ()
   (setf (fires *game*) nil))
 
+;;
+(defun my-sleep (m)
+  (let ((time1 (get-internal-real-time))
+	(time2 (get-internal-real-time )))
+    (loop :while (>= m (- time2 time1))
+       :do (setf time2 (get-internal-real-time)))))
+
+
+;;キー入力をnilにする
+(defun init-keystate ()
+  (with-slots (zkey left right up down spc) *keystate*
+    (setf zkey nil left nil right nil up nil down nil spc nil)))
+
+
+(defun add-wall ()
+  (when (>= (turn *game*) 300)
+    (multiple-value-bind (s a)
+	(floor (turn *game*) 300)
+      (when (> (length *falling-wall*) a)
+	(push (nth a *falling-wall*)
+	      (walls *game*))))))
+
+
 (defmethod gamekit:act ((app mogemberman))
-  ;;(update-player)
-  (delete-fires)
-  (update-coms) ;;移動
-  (update-bombs) ;;ボム更新&火
-  ;;(collision-fire-com)
-  (collision-fire-bomb)
-  (sleep 1))
+  (with-slots (zkey left right up down spc) *keystate*
+    (when (or (and (not (control *game*))
+		   (zerop (mod *frame* *frame-go*)))
+	      (and (control *game*)
+		   (or zkey left right up down spc)))
+      ;;(update-player)
+      (add-wall)
+      (delete-fires)
+      (update-coms) ;;移動
+      (update-bombs) ;;ボム更新&火
+      ;;(collision-fire-com)
+      (collision-fire-bomb)
+      (collision-item-com)
+      (collision-fire-item )
+      (init-keystate)
+      (incf (turn *game*))
+      (setf *frame* 0))
+    (incf *frame*)))
 
 (defmethod gamekit:post-initialize ((app mogemberman))
-  
+  (set-font)
   
   
   (create-field)
   ;;(create-player)
   ;;(create-players)
+  (bind-button :r :pressed
+	       (lambda () (init-data)))
   (bind-button :z :pressed
 	       (lambda () (setf (zkey *keystate*) t)))
   (bind-button :z :released 
 	       (lambda () (setf (zkey *keystate*) nil)))
+  (bind-button :left-shift :pressed
+	       (lambda ()
+		 (incf *gamespeed-count*)
+		 (when (> *gamespeed-count* 4)
+		   (setf *gamespeed-count* 0))
+		 (setf *frame-go* (nth *gamespeed-count* *gamespeed-list*))))
   (bind-button :right :pressed
 	       (lambda () (setf (right *keystate*) t)))
   (bind-button :left :pressed
@@ -514,4 +685,4 @@
 	;;*player* (make-instance 'player :pos (list 1 11))
 	*keystate* (make-instance 'keystate))
   (get-ai-from-txt)
-  (gamekit:start 'mogemberman)) ;;:viewport-resizable t))
+  (gamekit:start 'mogemberman :autoscaled t :viewport-resizable t)) ;;:viewport-resizable t))
